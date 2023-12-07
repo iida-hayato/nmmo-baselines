7a67e27947e126edb1949f58b2a6a9a09b230bc6
iida-hayato <hayato.iida.0213@gmail.com>
2023-12-07 10:35:44 +0900

    30 tick alived

--- curriculum_generation/manual_curriculum.py
+++ curriculum_generation/manual_curriculum.py
@@ -9,26 +9,30 @@ import nmmo.systems.item as Item
 import nmmo.systems.skill as Skill
 from nmmo.task import constraint as c
 from nmmo.task.base_predicates import (
-    AttainSkill,
-    BuyItem,
-    CanSeeAgent,
-    CanSeeGroup,
-    CanSeeTile,
-    ConsumeItem,
-    CountEvent,
-    EarnGold,
-    EquipItem,
-    GainExperience,
-    HarvestItem,
-    HoardGold,
-    InventorySpaceGE,
-    ListItem,
-    MakeProfit,
-    OccupyTile,
-    OwnItem,
-    ScoreHit,
-    SpendGold,
-    TickGE,
+  norm,
+  count,
+  AttainSkill,
+  BuyItem,
+  CanSeeAgent,
+  CanSeeGroup,
+  CanSeeTile,
+  ConsumeItem,
+  CountEvent,
+  EarnGold,
+  EquipItem,
+  GainExperience,
+  HarvestItem,
+  HoardGold,
+  InventorySpaceGE,
+  AllMembersWithinRange,
+  ListItem,
+  MakeProfit,
+  OccupyTile,
+  DefeatEntity,
+  OwnItem,
+  ScoreHit,
+  SpendGold,
+  TickGE, FullyArmed,
 )
 from nmmo.task.task_spec import TaskSpec, check_task_spec
 
@@ -43,14 +47,14 @@ ALL_ITEM = c.armour + c.weapons + c.tools + c.ammunition + c.consumables
 EQUIP_ITEM = c.armour + c.weapons + c.tools + c.ammunition
 HARVEST_ITEM = c.weapons + c.ammunition + c.consumables
 TOOL_FOR_SKILL = {
-    Skill.Melee: Item.Spear,
-    Skill.Range: Item.Bow,
-    Skill.Mage: Item.Wand,
-    Skill.Fishing: Item.Rod,
-    Skill.Herbalism: Item.Gloves,
-    Skill.Carving: Item.Axe,
-    Skill.Prospecting: Item.Pickaxe,
-    Skill.Alchemy: Item.Chisel,
+  Skill.Melee: Item.Spear,
+  Skill.Range: Item.Bow,
+  Skill.Mage: Item.Wand,
+  Skill.Fishing: Item.Rod,
+  Skill.Herbalism: Item.Gloves,
+  Skill.Carving: Item.Axe,
+  Skill.Prospecting: Item.Pickaxe,
+  Skill.Alchemy: Item.Chisel,
 }
 
 curriculum: List[TaskSpec] = []
@@ -58,164 +62,222 @@ curriculum: List[TaskSpec] = []
 # explore, eat, drink, attack any agent, harvest any item, level up any skill
 #   which can happen frequently
 most_essentials = [
-    "EAT_FOOD",
-    "DRINK_WATER",
+  "EAT_FOOD",
+  "DRINK_WATER",
 ]
-for event_code in most_essentials:
-  for cnt in range(1, 10):
-    curriculum.append(
-        TaskSpec(
-            eval_fn=CountEvent,
-            eval_fn_kwargs={"event": event_code, "N": cnt},
-            sampling_weight=100,
-        )
+
+
+def PracticeEating(gs, subject):
+  """The progress, the max of which is 1, should
+  * increase small for each eating
+  * increase big for the 1st and 3rd eating
+  * reach 1 with 10 eatings
+  """
+  num_eat = len(subject.event.EAT_FOOD)
+  progress = num_eat * 0.06
+  return norm(progress)  # norm is a helper function to normalize the value to [0, 1]
+
+
+curriculum.append(TaskSpec(eval_fn=PracticeEating, eval_fn_kwargs={}, sampling_weight=100))
+
+
+def PracticeDrinking(gs, subject):
+  num_drink = len(subject.event.DRINK_WATER)
+  progress = num_drink * 0.06
+  return norm(progress)
+
+
+curriculum.append(TaskSpec(eval_fn=PracticeDrinking, eval_fn_kwargs={}, sampling_weight=100))
+
+# for event_code in most_essentials:
+#   for cnt in range(1, 200):
+#     curriculum.append(
+#       TaskSpec(
+#         eval_fn=CountEvent,
+#         eval_fn_kwargs={"event": event_code, "N": cnt},
+#         sampling_weight=100,
+#       )
+#     )
+# attack npc
+for level in range(0, 10):
+  curriculum.append(
+    TaskSpec(
+      eval_fn=DefeatEntity,
+      eval_fn_kwargs={"agent_type": "npc", 'level': level, "num_agent": 20 - level},
+      sampling_weight=50 - level * 2,
     )
+  )
 
 essential_skills = [
-    "SCORE_HIT",
-    "PLAYER_KILL",
-    "HARVEST_ITEM",
-    "EQUIP_ITEM",
-    "CONSUME_ITEM",
-    "LEVEL_UP",
-    "EARN_GOLD",
-    "LIST_ITEM",
-    "BUY_ITEM",
+  "SCORE_HIT",
+  "PLAYER_KILL",
+  "HARVEST_ITEM",
+  "EQUIP_ITEM",
+  "CONSUME_ITEM",
+  "LEVEL_UP",
+  "EARN_GOLD",
+  "LIST_ITEM",
+  "BUY_ITEM",
 ]
 for event_code in essential_skills:
   for cnt in EVENT_NUMBER_GOAL:
     curriculum.append(
-        TaskSpec(
-            eval_fn=CountEvent,
-            eval_fn_kwargs={"event": event_code, "N": cnt},
-            sampling_weight=20,
-        )
+      TaskSpec(
+        eval_fn=CountEvent,
+        eval_fn_kwargs={"event": event_code, "N": cnt},
+        sampling_weight=20,
+      )
     )
 
 # item/market skills, which happen less frequently or should not do too much
 item_skills = [
-    "GIVE_ITEM",
-    "DESTROY_ITEM",
-    "GIVE_GOLD",
+  "GIVE_ITEM",
+  "DESTROY_ITEM",
+  "GIVE_GOLD",
 ]
 for event_code in item_skills:
   curriculum += [
-      TaskSpec(eval_fn=CountEvent, eval_fn_kwargs={
-               "event": event_code, "N": cnt})
-      for cnt in INFREQUENT_GOAL
+    TaskSpec(eval_fn=CountEvent, eval_fn_kwargs={
+      "event": event_code, "N": cnt})
+    for cnt in INFREQUENT_GOAL
   ]  # less than 10
+
+for resource in {m.Water, m.Foilage}:
+  curriculum.append(
+    TaskSpec(
+      eval_fn=CanSeeTile,
+      eval_fn_kwargs={"tile_type": resource},
+      sampling_weight=80,
+    )
+  )
 
 # find resource tiles
 for resource in m.Harvestable:
   curriculum.append(
-      TaskSpec(
-          eval_fn=CanSeeTile,
-          eval_fn_kwargs={"tile_type": resource},
-          sampling_weight=10,
-      )
+    TaskSpec(
+      eval_fn=CanSeeTile,
+      eval_fn_kwargs={"tile_type": resource},
+      sampling_weight=10,
+    )
   )
+
 
 # practice particular skill with a tool
 def PracticeSkillWithTool(gs, subject, skill, exp):
   return 0.3 * EquipItem(gs, subject, item=TOOL_FOR_SKILL[skill], level=1, num_agent=1) + \
-         0.7 * GainExperience(gs, subject, skill, exp, num_agent=1)
+    0.7 * GainExperience(gs, subject, skill, exp, num_agent=1)
+
 
 for skill in SKILLS:
   # level up a skill
   for level in LEVEL_GOAL[1:]:
     # since this is an agent task, num_agent must be 1
     curriculum.append(
-        TaskSpec(
-            eval_fn=AttainSkill,
-            eval_fn_kwargs={"skill": skill, "level": level, "num_agent": 1},
-            sampling_weight=10*(6 - level) if level < 6 else 5,
-        )
+      TaskSpec(
+        eval_fn=AttainSkill,
+        eval_fn_kwargs={"skill": skill, "level": level, "num_agent": 1},
+        sampling_weight=10 * (6 - level) if level < 6 else 5,
+      )
     )
-  
+
   # gain experience on particular skill
   for exp in EXP_GOAL:
     curriculum.append(
-        TaskSpec(
-            eval_fn=PracticeSkillWithTool,
-            eval_fn_kwargs={"skill": skill, "exp": exp},
-            sampling_weight=50,
-        )
+      TaskSpec(
+        eval_fn=PracticeSkillWithTool,
+        eval_fn_kwargs={"skill": skill, "exp": exp},
+        sampling_weight=50,
+      )
     )
 
 # stay alive ... like ... for 300 ticks
 # i.e., getting incremental reward for each tick alive as an individual or a team
 for num_tick in STAY_ALIVE_GOAL:
   curriculum.append(
-      TaskSpec(eval_fn=TickGE, eval_fn_kwargs={"num_tick": num_tick}))
+    TaskSpec(eval_fn=TickGE, eval_fn_kwargs={"num_tick": num_tick}, sampling_weight=norm(num_tick / 1000) * 100)
+  )
 
 # occupy the center tile, assuming the Medium map size
 # TODO: it'd be better to have some intermediate targets toward the center
 curriculum.append(TaskSpec(eval_fn=OccupyTile,
-                 eval_fn_kwargs={"row": 80, "col": 80}))
+                           eval_fn_kwargs={"row": 16, "col": 16}))
 
 # find the other team leader
 for target in ["left_team_leader", "right_team_leader"]:
   curriculum.append(TaskSpec(eval_fn=CanSeeAgent,
-                   eval_fn_kwargs={"target": target}))
+                             eval_fn_kwargs={"target": target}))
+
+curriculum.append(TaskSpec(eval_fn=AllMembersWithinRange,
+                           eval_fn_kwargs={"dist": 9}))
 
 # find the other team (any agent)
 for target in ["left_team", "right_team"]:
   curriculum.append(TaskSpec(eval_fn=CanSeeGroup,
-                   eval_fn_kwargs={"target": target}))
+                             eval_fn_kwargs={"target": target}))
 
 # practice specific combat style
 for style in COMBAT_STYLE:
   for cnt in EVENT_NUMBER_GOAL:
     curriculum.append(
-        TaskSpec(
-            eval_fn=ScoreHit,
-            eval_fn_kwargs={"combat_style": style, "N": cnt},
-            sampling_weight=5,
-        )
+      TaskSpec(
+        eval_fn=ScoreHit,
+        eval_fn_kwargs={"combat_style": style, "N": cnt},
+        sampling_weight=5,
+      )
     )
 
 # hoarding gold -- evaluated on the current gold
 for amount in EVENT_NUMBER_GOAL:
   curriculum.append(
-      TaskSpec(
-          eval_fn=HoardGold, eval_fn_kwargs={"amount": amount}, sampling_weight=10
-      )
+    TaskSpec(
+      eval_fn=HoardGold, eval_fn_kwargs={"amount": amount}, sampling_weight=10
+    )
   )
 
 # earning gold -- evaluated on the total gold earned by selling items
 for amount in EVENT_NUMBER_GOAL:
   curriculum.append(
-      TaskSpec(eval_fn=EarnGold, eval_fn_kwargs={
-               "amount": amount}, sampling_weight=10)
+    TaskSpec(eval_fn=EarnGold, eval_fn_kwargs={
+      "amount": amount}, sampling_weight=10)
   )
 
 # spending gold, by buying items
 for amount in EVENT_NUMBER_GOAL:
   curriculum.append(
-      TaskSpec(
-          eval_fn=SpendGold, eval_fn_kwargs={"amount": amount}, sampling_weight=5
-      )
+    TaskSpec(
+      eval_fn=SpendGold, eval_fn_kwargs={"amount": amount}, sampling_weight=5
+    )
   )
 
 # making profits by trading -- only buying and selling are counted
 for amount in EVENT_NUMBER_GOAL:
   curriculum.append(
-      TaskSpec(
-          eval_fn=MakeProfit, eval_fn_kwargs={"amount": amount}, sampling_weight=3
-      )
+    TaskSpec(
+      eval_fn=MakeProfit, eval_fn_kwargs={"amount": amount}, sampling_weight=3
+    )
   )
+
 
 # managing inventory space
 def PracticeInventoryManagement(gs, subject, space, num_tick):
   return InventorySpaceGE(gs, subject, space) * TickGE(gs, subject, num_tick)
 
+
+for num_agent in [1, 2]:
+  for level in LEVEL_GOAL:
+    for combat_style in COMBAT_STYLE:
+      curriculum.append(
+        TaskSpec(eval_fn=FullyArmed,
+                 eval_fn_kwargs={"combat_style": combat_style, "level": level, "num_agent": num_agent},
+                 sampling_weight=3 * num_agent))
+
 for space in [2, 4, 8]:
   curriculum += [
-      TaskSpec(
-          eval_fn=PracticeInventoryManagement,
-          eval_fn_kwargs={"space": space, "num_tick": num_tick},
-      )
-      for num_tick in STAY_ALIVE_GOAL
+    TaskSpec(
+      eval_fn=PracticeInventoryManagement,
+      eval_fn_kwargs={"space": space, "num_tick": num_tick},
+    )
+    for num_tick in STAY_ALIVE_GOAL
   ]
 
 # own item, evaluated on the current inventory
@@ -225,15 +287,15 @@ for item in ALL_ITEM:
     for quantity in ITEM_NUM_GOAL:
       if level + quantity <= 6 or quantity == 1:  # heuristic prune
         curriculum.append(
-            TaskSpec(
-                eval_fn=OwnItem,
-                eval_fn_kwargs={
-                    "item": item,
-                    "level": level,
-                    "quantity": quantity,
-                },
-                sampling_weight=4 - level if level < 4 else 1,
-            )
+          TaskSpec(
+            eval_fn=OwnItem,
+            eval_fn_kwargs={
+              "item": item,
+              "level": level,
+              "quantity": quantity,
+            },
+            sampling_weight=4 - level if level < 4 else 1,
+          )
         )
 
 # equip item, evaluated on the current inventory and equipment status
@@ -241,11 +303,11 @@ for item in EQUIP_ITEM:
   for level in LEVEL_GOAL:
     # agent task
     curriculum.append(
-        TaskSpec(
-            eval_fn=EquipItem,
-            eval_fn_kwargs={"item": item, "level": level, "num_agent": 1},
-            sampling_weight=4 - level if level < 4 else 1,
-        )
+      TaskSpec(
+        eval_fn=EquipItem,
+        eval_fn_kwargs={"item": item, "level": level, "num_agent": 1},
+        sampling_weight=4 - level if level < 4 else 1,
+      )
     )
 
 # consume items (ration, potion), evaluated based on the event log
@@ -255,15 +317,15 @@ for item in c.consumables:
     for quantity in ITEM_NUM_GOAL:
       if level + quantity <= 6 or quantity == 1:  # heuristic prune
         curriculum.append(
-            TaskSpec(
-                eval_fn=ConsumeItem,
-                eval_fn_kwargs={
-                    "item": item,
-                    "level": level,
-                    "quantity": quantity,
-                },
-                sampling_weight=4 - level if level < 4 else 1,
-            )
+          TaskSpec(
+            eval_fn=ConsumeItem,
+            eval_fn_kwargs={
+              "item": item,
+              "level": level,
+              "quantity": quantity,
+            },
+            sampling_weight=4 - level if level < 4 else 1,
+          )
         )
 
 # harvest items, evaluated based on the event log
@@ -273,15 +335,15 @@ for item in HARVEST_ITEM:
     for quantity in ITEM_NUM_GOAL:
       if level + quantity <= 6 or quantity == 1:  # heuristic prune
         curriculum.append(
-            TaskSpec(
-                eval_fn=HarvestItem,
-                eval_fn_kwargs={
-                    "item": item,
-                    "level": level,
-                    "quantity": quantity,
-                },
-                sampling_weight=4 - level if level < 4 else 1,
-            )
+          TaskSpec(
+            eval_fn=HarvestItem,
+            eval_fn_kwargs={
+              "item": item,
+              "level": level,
+              "quantity": quantity,
+            },
+            sampling_weight=4 - level if level < 4 else 1,
+          )
         )
 
 # list items, evaluated based on the event log
@@ -291,15 +353,15 @@ for item in ALL_ITEM:
     for quantity in ITEM_NUM_GOAL:
       if level + quantity <= 6 or quantity == 1:  # heuristic prune
         curriculum.append(
-            TaskSpec(
-                eval_fn=ListItem,
-                eval_fn_kwargs={
-                    "item": item,
-                    "level": level,
-                    "quantity": quantity,
-                },
-                sampling_weight=4 - level if level < 4 else 1,
-            )
+          TaskSpec(
+            eval_fn=ListItem,
+            eval_fn_kwargs={
+              "item": item,
+              "level": level,
+              "quantity": quantity,
+            },
+            sampling_weight=4 - level if level < 4 else 1,
+          )
         )
 
 # buy items, evaluated based on the event log
@@ -309,15 +371,15 @@ for item in ALL_ITEM:
     for quantity in ITEM_NUM_GOAL:
       if level + quantity <= 6 or quantity == 1:  # heuristic prune
         curriculum.append(
-            TaskSpec(
-                eval_fn=BuyItem,
-                eval_fn_kwargs={
-                    "item": item,
-                    "level": level,
-                    "quantity": quantity,
-                },
-                sampling_weight=4 - level if level < 4 else 1,
-            )
+          TaskSpec(
+            eval_fn=BuyItem,
+            eval_fn_kwargs={
+              "item": item,
+              "level": level,
+              "quantity": quantity,
+            },
+            sampling_weight=4 - level if level < 4 else 1,
+          )
         )
 
 if __name__ == "__main__":
@@ -328,6 +390,7 @@ if __name__ == "__main__":
   import numpy as np
   import psutil
 
+
   @contextmanager
   def create_pool(num_proc):
     pool = mp.Pool(processes=num_proc)
@@ -335,8 +398,9 @@ if __name__ == "__main__":
     pool.close()
     pool.join()
 
+
   # 1609 task specs: divide the specs into chunks
-  num_workers = round(psutil.cpu_count(logical=False)*0.7)
+  num_workers = round(psutil.cpu_count(logical=False) * 0.7)
   spec_chunks = np.array_split(curriculum, num_workers)
   with create_pool(num_workers) as pool:
     pool.map(check_task_spec, spec_chunks)
